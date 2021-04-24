74f9ba34bd7d2ddf5fd019ea8429cf147f97bb28
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2021-04-24 11:30:41 +0300

    Initial

---
+++ TrainingCoreExample/Database/ContactRequestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCoreExample.Database
+{
+    public class ContactRequestDbContext : DbContext
+    {
+        public ContactRequestDbContext(DbContextOptions<ContactRequestDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactRequest> ContactRequests { get; set; }
+    }
+}
